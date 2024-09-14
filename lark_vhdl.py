ad975b754e34cbfa0b2d76aa175cb4acc96e2feb
Petr Nohavica <petr.nohavica@gmail.com>
2024-09-14 19:37:11 +0200

    vhdl.lark: accepted by lark

--- lark_vhdl.py
+++ lark_vhdl.py
@@ -1,26 +1,10 @@
+from pprint import pprint
 from lark import Lark
 
-with open("vhdl-2008.ebnf") as f:
-    s = f.read()
 
-vhdl_grammar = """
-    start: instruction+
+with open("vhdl.lark") as f:
+    vhdl_grammar = f.read()
 
-    instruction: MOVEMENT NUMBER            -> movement
-               | "c" COLOR [COLOR]          -> change_color
-               | "fill" code_block          -> fill
-               | "repeat" NUMBER code_block -> repeat
+#pprint(vhdl_grammar)
 
-    code_block: "{" instruction+ "}"
-
-    MOVEMENT: "f"|"b"|"l"|"r"
-    COLOR: LETTER+
-
-    %import common.LETTER
-    %import common.INT -> NUMBER
-    %import common.WS
-    %ignore WS
-"""
-
-
-parser = Lark(s)
+parser = Lark(vhdl_grammar)
